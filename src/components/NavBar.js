5cd3144d74cfae16721fd36365d7bc4b6d19b07c
arzamaan <arzamaanh@gmail.com>
2022-07-01 21:56:45 +0530

    Revert "Added links to navbar menus"
    This reverts commit 46fbe81dd48c9d3d594b3adc03047516e778b558.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,168 +10,156 @@ function NavBar() {
     <>
       <nav className="navbar">
         <div className="comp-logo">
-          <NavLink exact to="/"
-            activeClassName="active"
-            className="nav-links"
-            onClick={handleClick}>
+        <NavLink exact to="/" 
+         activeClassName="active"
+         className="nav-links"
+         onClick={handleClick}>
             <img src={logo} style={{
               width: 110,
-              height: 110,
+              height:110,
               verticalAlign: "center",
-              marginTop: 5,
-
-            }} />
-            <i className="logotext" style={{ position: 'relative', top: -10 }}>P SHIKSHA 4.0 </i>
-          </NavLink>
-
-        </div>
-
+              marginTop:5,
+              
+            }}  /> 
+      <i className="logotext" style={{position: 'relative',top: -10}}>P SHIKSHA 4.0 </i>
+         </NavLink>
+        
+         </div>
+         
         {/* <div className="nav-container"> */}
         <div className="innav">
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <div class="dropdown">
-                <button class="dropbtn">Services
-                  <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                  <div class="row">
-                    <div class="column">
-                      <h3>Engineering</h3>
-                      <NavLink
-                        exact to="/placement"
-                        onClick={handleClick}
-                      >
-                        Placement Preparation
-                      </NavLink>
-                      <NavLink
-                        exact to="/assignment"
-                        onClick={handleClick}
-                      >
-                        Assignment Making
-                      </NavLink>
-                      <NavLink
-                        exact to="/projects"
-                        onClick={handleClick}
-                      >
-                        Major/Minor project
-                      </NavLink>
-                      <NavLink
-                        exact to="/cat-xat-coaching"
-                        onClick={handleClick}
-                      >
-                        CAT/XAT Coaching
-                      </NavLink>
-                      <NavLink
-                        exact to="/ielts-visa"
-                        onClick={handleClick}
-                      >
-                        IELTS, Visa, Immigration
-                      </NavLink>
-                      <NavLink
-                        exact to="/internship-service"
-                        onClick={handleClick}
-                      >
-                       Get Internship
-                      </NavLink>
-
-                    </div>
-                    <div class="column">
-                      <h3>Exams</h3>
-                      <NavLink
-                        exact to="/iit"
-                        onClick={handleClick}
-                      >
-                        IIT
-                      </NavLink>
-                      <NavLink
-                        exact to="/HomeTution"
-                        onClick={handleClick}
-                      >
-                        NEET
-                      </NavLink>
-                    </div>
-                    <div class="column">
-                      <h3>Abroad</h3>
-                      <NavLink
-                        exact to="/ielts"
-                        onClick={handleClick}
-                      >
-                        IELTS
-
-                      </NavLink>
-                      <NavLink
-                        exact to="/overseas"
-                        onClick={handleClick}
-                      >
-                        Overseas
-                      </NavLink>
-
-                    </div>
-                    <div class="column">
-                      <h3>Students</h3>
-                      <NavLink exact to="/internship"
-                        onClick={handleClick}
-                      >
-                        Internship
-                      </NavLink>
-                      <NavLink
-                        exact to="/vaani"
-                        onClick={handleClick}
-                      >
-                        Training cum Internship
-                      </NavLink>
-                      <NavLink
-                        exact to="/ambassador"
-                        onClick={handleClick}
-                      >
-                        Campus Ambassador
-                      </NavLink>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li className="nav-item">
-
-              <div class="dropdown1">
-                <button class="dropbtn1">More
-                  <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content1">
-                  <NavLink
-                    exact to="/HomeTution"
-                    onClick={handleClick}
-                  >
-                    Extra Curricular
-                  </NavLink>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-                </div>
-              </div>
-
-            </li>
-            <li>
-              <div class="dropdown1">
-                <button class="dropbtn1">
-                  <NavLink
-                    style={{ textDecoration: 'none' }}
-                    exact to="/login"
-                    onClick={handleClick}
-                  >
-                    <p style={{ color: "white" }}>Login
-                    </p>
-                  </NavLink>
-
-
-                </button>
-              </div>
-            </li>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+      <li className="nav-item">
+      <div class="dropdown">
+            <button class="dropbtn">Services
+            <i class="fa fa-caret-down"></i>
+            </button>
+        <div class="dropdown-content">
+            <div class="row">
+              <div class="column">
+          <h3>Engineering</h3>
+          <NavLink
+                exact to="/iit"
+                onClick={handleClick}
+              >
+              Placement Preparation
+              </NavLink>
+              <NavLink
+                exact to="/placement"
+                onClick={handleClick}
+              >
+              professional CV making
+              </NavLink>
+              <NavLink
+                exact to="/iit"
+                onClick={handleClick}
+              >
+              Major/Minor project
+              </NavLink>
+              <NavLink
+                exact to="/projects"
+                onClick={handleClick}
+              >
+              Plag-free reports
+              </NavLink>
+        
+        </div>
+            <div class="column">
+            <h3>Exams</h3>
+            <NavLink
+                exact to="/iit"
+                onClick={handleClick}
+              >
+              IIT
+              </NavLink>
+              <NavLink
+                exact to="/HomeTution"
+                onClick={handleClick}
+              >
+              NEET
+              </NavLink>
+          </div>
+          <div class="column">
+          <h3>Abroad</h3>
+          <NavLink
+                exact to="/ielts"
+                onClick={handleClick}
+              >
+              IELTS
+              
+              </NavLink> 
+              <NavLink
+                exact to="/overseas"
+                onClick={handleClick}
+              >
+              Overseas
+              </NavLink> 
+          
+        </div>
+        <div class="column">
+          <h3>Students</h3>
+          <NavLink exact to="/internship" 
+            onClick={handleClick}
+            >
+            Internship
+            </NavLink>
+            <NavLink
+                exact to="/vaani"
+                onClick={handleClick}
+              >
+              Training cum Internship
+              </NavLink>
+              <NavLink
+                exact to="/ambassador"
+                onClick={handleClick}
+              >
+              Campus Ambassador
+              </NavLink>
+        </div>
+      </div>
+    </div>
+  </div> 
+          </li>
+          <li className="nav-item">
+           
+           <div class="dropdown1">
+            <button class="dropbtn1">More
+            <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content1">
+            <NavLink
+                exact to="/HomeTution"
+                onClick={handleClick}
+              >
+              Extra Curricular
+              </NavLink>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+            </div>
+            </div>
+          
+          </li>
+          <li>
+          <div class="dropdown1">
+            <button class="dropbtn1">
+            <NavLink
+            style={{textDecoration: 'none'}}
+                exact to="/login"
+                onClick={handleClick}
+              >
+              <p style={{color:"white"}}>Login
+              </p>
+              </NavLink>
+          
+            
+            </button>
+            </div>
+              </li>
           </ul>
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
           </div>
-        </div>
+          </div>
       </nav>
     </>
   );

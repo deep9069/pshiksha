9d4f2b171df3bdded361d43b21518144c70af46a
Pratyush <54001208+pratyushk039@users.noreply.github.com>
2022-07-27 13:31:06 +0530

    backend folder

---
+++ backend/schemas/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const path = require('path');
+//const AVATAR_PATH = path.join('/uploads/users/avatars');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    }});
+
+
+// let storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, path.join(__dirname, '..', AVATAR_PATH));
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.fieldname + '-' + Date.now());
+//     }
+//   });
+
+
+// static
+// userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+// userSchema.statics.avatarPath = AVATAR_PATH;
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

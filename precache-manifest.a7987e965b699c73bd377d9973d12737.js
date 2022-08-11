5f6d2f9d9f6039ca984aff9e0b49316aa78a05e3
Deepak <deep7982129926@gmail.com>
2022-08-11 13:48:53 +0530

    Updates

---
+++ precache-manifest.a7987e965b699c73bd377d9973d12737.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462eb21fc79b79e1e33a8be9eca7707f",
+    "url": "/pshiksha/index.html"
+  },
+  {
+    "revision": "3ab641b420d2bd64a9d1",
+    "url": "/pshiksha/static/css/2.38d19ba7.chunk.css"
+  },
+  {
+    "revision": "2a712dc23302349cbe2a",
+    "url": "/pshiksha/static/css/main.72840eaa.chunk.css"
+  },
+  {
+    "revision": "3ab641b420d2bd64a9d1",
+    "url": "/pshiksha/static/js/2.08c3ce81.chunk.js"
+  },
+  {
+    "revision": "bc91af54090c216c2661cdef3cd16b3d",
+    "url": "/pshiksha/static/js/2.08c3ce81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d99dd534bceafad39c",
+    "url": "/pshiksha/static/js/3.b2cfcfec.chunk.js"
+  },
+  {
+    "revision": "2a712dc23302349cbe2a",
+    "url": "/pshiksha/static/js/main.bfb26895.chunk.js"
+  },
+  {
+    "revision": "327d8ac5d1665d626ed2",
+    "url": "/pshiksha/static/js/runtime-main.a53100a6.js"
+  },
+  {
+    "revision": "401adf446f73c55c17ad93ce503ac4d1",
+    "url": "/pshiksha/static/media/10502.401adf44.jpg"
+  },
+  {
+    "revision": "2ab096df76cfc0443e01d2cb76408fb4",
+    "url": "/pshiksha/static/media/15410.2ab096df.jpg"
+  },
+  {
+    "revision": "9b4ed8be646e4913da3e730bc854f473",
+    "url": "/pshiksha/static/media/Arjun.9b4ed8be.png"
+  },
+  {
+    "revision": "92bfe72b01cac7281ae8866862db4ba3",
+    "url": "/pshiksha/static/media/NEET_Fee_Package.92bfe72b.pdf"
+  },
+  {
+    "revision": "5ff60fa9a5dfa3c3f8ed565ecee13f8c",
+    "url": "/pshiksha/static/media/Online_test_series.5ff60fa9.pdf"
+  },
+  {
+    "revision": "706c48359274d4da9fd773272775ebf6",
+    "url": "/pshiksha/static/media/Online_test_series_neet.706c4835.pdf"
+  },
+  {
+    "revision": "c75b73914e4e65a4d19dd9cebf0ba0d0",
+    "url": "/pshiksha/static/media/Shiksha_IIT-JEE.c75b7391.pdf"
+  },
+  {
+    "revision": "37203b55e4e0e4416d9104cfe190defb",
+    "url": "/pshiksha/static/media/Shiksha_NEET.37203b55.pdf"
+  },
+  {
+    "revision": "55cf764bd8d5d89cb6f3c28220107534",
+    "url": "/pshiksha/static/media/Study_Material.55cf764b.pdf"
+  },
+  {
+    "revision": "c266f71ca17cabffb652a96bd6d39405",
+    "url": "/pshiksha/static/media/Study_Material_Neet.c266f71c.pdf"
+  },
+  {
+    "revision": "e1c6876857df78b79f90c7e1ca95717e",
+    "url": "/pshiksha/static/media/akshat.e1c68768.png"
+  },
+  {
+    "revision": "5fc7385a963719ffdae03976efad428f",
+    "url": "/pshiksha/static/media/arnav.5fc7385a.png"
+  },
+  {
+    "revision": "6395de9881313d9624022c222c0276d5",
+    "url": "/pshiksha/static/media/arzamaan.6395de98.png"
+  },
+  {
+    "revision": "165b35b881c2c1d93d2f320747188413",
+    "url": "/pshiksha/static/media/ashish.165b35b8.png"
+  },
+  {
+    "revision": "d1180ce170da8323befc59c2634954cd",
+    "url": "/pshiksha/static/media/ashish_angel.d1180ce1.png"
+  },
+  {
+    "revision": "132042d659e06a8ace7cb720cdea5230",
+    "url": "/pshiksha/static/media/ashwin.132042d6.png"
+  },
+  {
+    "revision": "757406d2b7600c0236a176cca56c7023",
+    "url": "/pshiksha/static/media/aus.757406d2.png"
+  },
+  {
+    "revision": "16566526e9189ad9ff5cb36dd606bfa0",
+    "url": "/pshiksha/static/media/aus1.16566526.png"
+  },
+  {
+    "revision": "00f16c594f955f65a6f4bba2147bcfcf",
+    "url": "/pshiksha/static/media/aus2.00f16c59.png"
+  },
+  {
+    "revision": "ac318efe158ea75c400f4cd6d999a241",
+    "url": "/pshiksha/static/media/aus3.ac318efe.png"
+  },
+  {
+    "revision": "26ee42b06058eb1ba7cab3686dff20d9",
+    "url": "/pshiksha/static/media/austria.26ee42b0.png"
+  },
+  {
+    "revision": "88052c7d8534ed79087468a40857d024",
+    "url": "/pshiksha/static/media/austria1.88052c7d.png"
+  },
+  {
+    "revision": "8f449a10d1c171683ed348d842aa00a5",
+    "url": "/pshiksha/static/media/bel.8f449a10.png"
+  },
+  {
+    "revision": "1952bf0da8e6b0f8c03c97ddb69ffb5d",
+    "url": "/pshiksha/static/media/c_ambas.1952bf0d.png"
+  },
+  {
+    "revision": "d7e9a25c4236e469d5020d93804360c5",
+    "url": "/pshiksha/static/media/c_ielts.d7e9a25c.png"
+  },
+  {
+    "revision": "37e84c14d0fcaaf8fc67620fb4453dca",
+    "url": "/pshiksha/static/media/c_iit.37e84c14.png"
+  },
+  {
+    "revision": "b51d01ae0bb7240799c7d9f68b8da5b5",
+    "url": "/pshiksha/static/media/c_intern.b51d01ae.png"
+  },
+  {
+    "revision": "f6765122df7604281d3de4a240bb68e2",
+    "url": "/pshiksha/static/media/c_neet.f6765122.png"
+  },
+  {
+    "revision": "69c6c4951b7f4d98ee417d3c6b90eab5",
+    "url": "/pshiksha/static/media/c_over.69c6c495.png"
+  },
+  {
+    "revision": "4c7430d02497356920ca5c9129e5caaf",
+    "url": "/pshiksha/static/media/c_tuition.4c7430d0.png"
+  },
+  {
+    "revision": "ea686e2aab0c88a5f757852d9b1a4467",
+    "url": "/pshiksha/static/media/can.ea686e2a.png"
+  },
+  {
+    "revision": "b84aa1d16a1493517b86a0e1f856b3ff",
+    "url": "/pshiksha/static/media/can1.b84aa1d1.png"
+  },
+  {
+    "revision": "e71c2cf485e172cd854d7fbf203ac44e",
+    "url": "/pshiksha/static/media/can2.e71c2cf4.png"
+  },
+  {
+    "revision": "5dd3719cf379c4ea43c0402a96da6826",
+    "url": "/pshiksha/static/media/can3.5dd3719c.png"
+  },
+  {
+    "revision": "11586eaef818179cf5e16ccdb4002603",
+    "url": "/pshiksha/static/media/can4.11586eae.png"
+  },
+  {
+    "revision": "aefaa84264e5fdf2ade309fed3b2b370",
+    "url": "/pshiksha/static/media/chanpreet.aefaa842.png"
+  },
+  {
+    "revision": "f4296ed93a3dbeac11105df47dc8d9d0",
+    "url": "/pshiksha/static/media/coursesimg.f4296ed9.png"
+  },
+  {
+    "revision": "6935ad5edd6b52f87d0ebadbff78da37",
+    "url": "/pshiksha/static/media/deepak.6935ad5e.png"
+  },
+  {
+    "revision": "5456f175bedbab292434abd1aaafc2c2",
+    "url": "/pshiksha/static/media/deepanshu.5456f175.png"
+  },
+  {
+    "revision": "2f20166af4d416aa681782e5c395d5a9",
+    "url": "/pshiksha/static/media/dtu logo.2f20166a.jpg"
+  },
+  {
+    "revision": "b32634fd2dd0ec204ce1c4dc364c5f84",
+    "url": "/pshiksha/static/media/dtunsutiiit.b32634fd.png"
+  },
+  {
+    "revision": "cde230cc2c29fece6cf7f176f13bb015",
+    "url": "/pshiksha/static/media/fra.cde230cc.png"
+  },
+  {
+    "revision": "6f62300f39d35a4810211d51a7d765e9",
+    "url": "/pshiksha/static/media/ger.6f62300f.png"
+  },
+  {
+    "revision": "23985840fe360230004bf2d3c66fbede",
+    "url": "/pshiksha/static/media/idfc.23985840.webp"
+  },
+  {
+    "revision": "abafef58656e8f43b3f5f4effaab1435",
+    "url": "/pshiksha/static/media/ielts-fee-structure.abafef58.pdf"
+  },
+  {
+    "revision": "b5059c1b8bf462c78e255c94ebc6b961",
+    "url": "/pshiksha/static/media/iit-jee_Fee_Package.b5059c1b.pdf"
+  },
+  {
+    "revision": "263c915b7c868dbc943c92d6a61f092a",
+    "url": "/pshiksha/static/media/ire.263c915b.png"
+  },
+  {
+    "revision": "5d579cf0a0f336d0a1c58cde4b9ec03f",
+    "url": "/pshiksha/static/media/jitesh.5d579cf0.png"
+  },
+  {
+    "revision": "43b47b13f4722a11a2203aba63906e13",
+    "url": "/pshiksha/static/media/logo.43b47b13.png"
+  },
+  {
+    "revision": "bc2a4e8fb977c16b198eb6000cb7130f",
+    "url": "/pshiksha/static/media/naveen.bc2a4e8f.png"
+  },
+  {
+    "revision": "713393e8755566a8109942b9c17938a4",
+    "url": "/pshiksha/static/media/newz.713393e8.png"
+  },
+  {
+    "revision": "e0f0735fc27e9a1ce7b41e6c01f4e122",
+    "url": "/pshiksha/static/media/nontech.e0f0735f.png"
+  },
+  {
+    "revision": "8b38a350670f49ecde630f7bbf4164ef",
+    "url": "/pshiksha/static/media/nsut logo.8b38a350.png"
+  },
+  {
+    "revision": "f434ed14dc058d07d41bba5192f4ff18",
+    "url": "/pshiksha/static/media/pratyush.f434ed14.png"
+  },
+  {
+    "revision": "f988da522bd55a54ec2b84adbe6a41a9",
+    "url": "/pshiksha/static/media/priyanka.f988da52.png"
+  },
+  {
+    "revision": "5f188189754eae29a731b1099be8c28e",
+    "url": "/pshiksha/static/media/razorpay.5f188189.webp"
+  },
+  {
+    "revision": "1a0ece06ad5792106e5f2fc77030179b",
+    "url": "/pshiksha/static/media/rishab_kumar.1a0ece06.png"
+  },
+  {
+    "revision": "c05adf2f0630b13abc84ea197a1272e1",
+    "url": "/pshiksha/static/media/rishab_rajpoot.c05adf2f.png"
+  },
+  {
+    "revision": "7e0393c5fb50f2a3281ce990ed697590",
+    "url": "/pshiksha/static/media/roshan.7e0393c5.png"
+  },
+  {
+    "revision": "77c1b98b2eecf1ceed3374082e675799",
+    "url": "/pshiksha/static/media/salman.77c1b98b.png"
+  },
+  {
+    "revision": "664211a5eeda1da36c6f94d1179e4461",
+    "url": "/pshiksha/static/media/samvat.664211a5.png"
+  },
+  {
+    "revision": "14c3ca28e172f254ef7cd775d40cb950",
+    "url": "/pshiksha/static/media/scammaster.14c3ca28.webp"
+  },
+  {
+    "revision": "aac613399c955aa29a9f6fa338a99071",
+    "url": "/pshiksha/static/media/sin.aac61339.png"
+  },
+  {
+    "revision": "e7af202a399f7921e79291b425a1a2a8",
+    "url": "/pshiksha/static/media/sin1.e7af202a.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/pshiksha/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/pshiksha/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/pshiksha/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/pshiksha/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "988d0ad8374446065a2663c8e394e566",
+    "url": "/pshiksha/static/media/suraj.988d0ad8.png"
+  },
+  {
+    "revision": "a393543c68c4cf653c70ddd37140f463",
+    "url": "/pshiksha/static/media/svg1.a393543c.png"
+  },
+  {
+    "revision": "a48661710261167e238f888fb45b2e40",
+    "url": "/pshiksha/static/media/swe.a4866171.png"
+  },
+  {
+    "revision": "d1c3353c2344c538fd53f8c8150dd20c",
+    "url": "/pshiksha/static/media/tech.d1c3353c.png"
+  },
+  {
+    "revision": "4c00eab47b3ed8abe9c459925e362bb2",
+    "url": "/pshiksha/static/media/uk.4c00eab4.png"
+  },
+  {
+    "revision": "1403d9aab4d948128b2c83eb219f7f3d",
+    "url": "/pshiksha/static/media/uk1.1403d9aa.png"
+  },
+  {
+    "revision": "14cc557227dd47ea61a6683ce52069b6",
+    "url": "/pshiksha/static/media/uk2.14cc5572.png"
+  },
+  {
+    "revision": "fbd7fcf85d58b68005d8859756b60442",
+    "url": "/pshiksha/static/media/usa.fbd7fcf8.png"
+  },
+  {
+    "revision": "439955fe16a6532b79be732739cd5867",
+    "url": "/pshiksha/static/media/usa1.439955fe.png"
+  },
+  {
+    "revision": "ec9df9a54b56f00f16ff0f240f998657",
+    "url": "/pshiksha/static/media/usa2.ec9df9a5.png"
+  },
+  {
+    "revision": "83dc7c7efb059d857da1f8d02380b26b",
+    "url": "/pshiksha/static/media/vedant.83dc7c7e.png"
+  },
+  {
+    "revision": "1c01fd51f2a588c4f4f707787e74b1d8",
+    "url": "/pshiksha/static/media/yellow and black.1c01fd51.jpg"
+  },
+  {
+    "revision": "f962fd2d370aaff5824923fa886e6e45",
+    "url": "/pshiksha/static/media/you.f962fd2d.png"
+  }
+]);
